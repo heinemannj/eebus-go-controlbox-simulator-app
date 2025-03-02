8cbac6c6555bf049f4df871c5f61ee39a1111373
Joerg Heinemann <heinemannj66@gmail.com>
2025-03-02 09:23:24 +0100

    Initial commit

--- install/debian/eebus-go-apps.sh
+++ install/debian/eebus-go-apps.sh
@@ -9,7 +9,7 @@ APP="eebus-go-apps"
 var_tags="eebus;hems;automation"
 var_cpu="1"
 var_ram="1024"
-var_disk="4"
+var_disk="16"
 var_os="debian"
 var_version="12"
 var_unprivileged="1"
@@ -23,30 +23,30 @@ function update_script() {
   header_info
   check_container_storage
   check_container_resources
-#  if [[ ! -f /etc/apt/sources.list.d/evcc-stable.list ]]; then
-#    msg_error "No ${APP} Installation Found!"
-#    exit
-#  fi
-  msg_info "Updating EEBUS-go-apps LXC"
+  #  if [[ ! -f /etc/apt/sources.list.d/evcc-stable.list ]]; then
+  #    msg_error "No ${APP} Installation Found!"
+  #    exit
+  #  fi
+  msg_info "Updating eebus-go-apps LXC"
 
-  $STD cd /opt
+  ##$STD cd /opt
 
-  $STD apt update -y
-  $STD apt upgrade -y
+  ##$STD apt update -y
+  ##$STD apt upgrade -y
 
   # Install syslog-ng
-  $STD apt install syslog-ng -y
+  ##$STD apt install syslog-ng -y
   # $STD systemctl daemon-reload
   # $STD systemctl enable --now syslog-ng
 
   # Install NodeJS and NPM
-  $STD apt install nodejs -y
-  $STD apt install npm -y
+  ##$STD apt install nodejs -y
+  ##$STD apt install npm -y
 
   # Install GO
-  $STD wget -q https://golang.org/dl/go1.23.6.linux-amd64.tar.gz
-  $STD rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.6.linux-amd64.tar.gz
-  $STD rm -rf /opt/go1.23.6.linux-amd64.tar.gz
+  ##$STD wget -q https://golang.org/dl/go1.23.6.linux-amd64.tar.gz
+  ##$STD rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.6.linux-amd64.tar.gz
+  ##$STD rm -rf /opt/go1.23.6.linux-amd64.tar.gz
 
   # `nano .profile`
   # `export PATH=$PATH:/usr/local/go/bin
@@ -57,12 +57,12 @@ function update_script() {
   #`source ~/.profile`
 
   # Install EEBUS-Go Apps
-  $STD wget -q https://github.com/heinemannj/eebus-go-controlbox-simulator-app/archive/refs/heads/main.zip
-  $STD mv eebus-go-controlbox-simulator-app-main.zip eebus-go-apps.zip
-  $STD unzip eebus-go-apps.zip
+  ##$STD wget -q https://github.com/heinemannj/eebus-go-controlbox-simulator-app/archive/refs/heads/main.zip
+  ##$STD mv eebus-go-controlbox-simulator-app-main.zip eebus-go-apps.zip
+  ##$STD unzip eebus-go-apps.zip
 
   # Configure systemd
-  $STD cp -r /opt/eebus-go-apps/install/debian/systemd/* /usr/lib/systemd/system
+  ##$STD cp -r /opt/eebus-go-apps/install/debian/systemd/* /usr/lib/systemd/system
   #$STD systemctl daemon-reload
   #$STD systemctl enable --now eebus-go-controlbox_evcc.service
   #$STD systemctl enable --now eebus-go-controlbox_eebus-go-hems.service
@@ -71,7 +71,7 @@ function update_script() {
   #$STD systemctl daemon-reload
 
   # Configure syslog-ng
-  $STD cp -r /opt/eebus-go-apps/install/debian/syslog-ng/conf.d /etc/syslog-ng
+  ##$STD cp -r /opt/eebus-go-apps/install/debian/syslog-ng/conf.d /etc/syslog-ng
 
   msg_ok "Updated Successfully"
   exit
